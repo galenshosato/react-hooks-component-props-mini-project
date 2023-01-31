69c1514c3d9254336653ff125603b8498bda098d
Galen Sho Sato <galen.sato@gmail.com>
2023-01-31 16:59:18 -0500

    Finished the Bonus Deliverable

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,12 +4,24 @@ function Article({
     title,
     date = 'January 1, 1970',
     preview,
+    minutes,
 }) {
+    let minutesEmojis = ''
+    if(minutes < 30) {
+        let roundedMins = Math.ceil(minutes / 5)
+        minutesEmojis = '☕️'.repeat(roundedMins)
+    } else {
+        let roundedMins = Math.ceil(minutes / 10)
+        minutesEmojis = '🍱'.repeat(roundedMins)
+    }
+
+
     return(
         <article>
             <h3>{title}</h3>
             <small>{date}</small>
             <p>{preview}</p>
+            <p>{minutesEmojis}{minutes} minutes</p>
         </article>
 
     )
